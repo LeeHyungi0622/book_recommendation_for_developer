e53e749e9e87a66d3c3a9560e0ce1143180c2711
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-02-13 13:14:42 +0900

    refactor: refactor index.js and Router.js
    - Update the location of app component
    - Call screen components using lazy
    - Split the code using react lazy and suspense

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,22 +1,31 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./Header";
-import IT from "../Screens/IT";
-import Development from "../Screens/Development";
-import Search from "../Screens/Search";
+import Loading from "./Loading";
+// import IT from "../Screens/IT";
+// import Development from "../Screens/Development";
+// import Search from "../Screens/Search";
+
+// screen components를 lazy하게 가져오기
+const IT = lazy(() => import("../Screens/IT"));
+const Development = lazy(() => import("../Screens/Development"));
+const Search = lazy(() => import("../Screens/Search"));
 
 // Router component
 const Router = () => (
     <BrowserRouter>
         <>
             <Header/>
-            <Switch>
-                <Route exact path="/" component={IT}>
-                    <Redirect to="/it"/>
-                </Route>
-                <Route path="/it" component={IT} />
-                <Route path="/development" component={Development}/>
-                <Route path="/search" component={Search} />
-            </Switch>
+            <Suspense fallback={<Loading />}>
+                <Switch>
+                    <Route exact path="/" component={IT}>
+                        <Redirect to="/it"/>
+                    </Route>
+                    <Route path="/it" component={IT} />
+                    <Route path="/development" component={Development}/>
+                    <Route path="/search" component={Search} />
+                </Switch>
+            </Suspense>
         </>
     </BrowserRouter>
 )
